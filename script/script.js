534ca1b7aea7705c35d65a5f2f440a379f089d9a
Slava Lebedintsev <xcarvedx@yandex.ru>
2020-05-30 21:51:11 +0300

    feat: сделаны попапы превью и открытия формы добавления

--- script/script.js
+++ script/script.js
@@ -1,10 +1,55 @@
+const initialCards = [
+  {
+    name: 'Онежское озеро',
+    link: 'https://images.unsplash.com/photo-1543699936-c901ddbf0c05?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=633&q=80'
+  },
+  {
+    name: 'Тулиновка',
+    link: 'https://images.unsplash.com/photo-1516128935666-9742cf27e24c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=675&q=80'
+  },
+  {
+    name: 'Выборг',
+    link: 'https://images.unsplash.com/photo-1536012354193-8bb300dc3ce6?ixlib=rb-1.2.1&auto=format&fit=crop&w=676&q=80'
+  },
+  {
+    name: 'Хийденсельга',
+    link: 'https://images.unsplash.com/photo-1559029884-e95924923629?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80'
+  },
+  {
+    name: 'Самарская лука',
+    link: 'https://images.unsplash.com/photo-1579987801223-f3823e4f536b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1267&q=80'
+  },
+  {
+    name: 'Ладога',
+    link: 'https://images.unsplash.com/photo-1547846218-c982107d30f2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1286&q=80'
+  }
+];
+
 const container = document.querySelector('.page');
-const editButton = container.querySelector('.profile__edit');
 const popup = container.querySelector('.popup');
 const popupExit = container.querySelector('.popup__close');
+const popupPreview = container.querySelector('.popup_preview');
+const popupImg = container.querySelector('.popup__image');
+const popupAdd = container.querySelector('.popup_add');
+const popupCreate = container.querySelector('.popup__create');
+
+const formAdd = document.forms['popup_add'];
+
+// const popupLink = container.querySelector('.popup__link');
+// const popupName = container.querySelector('.popup__name');
+const popupExitAdd = container.querySelector('.popup__close_add');
+const popupSubtitle = container.querySelector('.popup__subtitle');
+const popupExitPreview = container.querySelector('.popup__close-preview');
+const formElement = document.forms['popup'];
+
+const editButton = container.querySelector('.profile__edit');
 const formTitle = container.querySelector('.profile__title');
 const formSubtitle = container.querySelector('.profile__subtitle');
-const formElement = document.forms['popup'];
+const addButton = container.querySelector('.profile__button');
+
+const elements = document.querySelector('.elements');
+const itemTemplate = document.querySelector('.template__item').content;
+
 
 //объявляем константы для полей ввода
 const nameInput = formElement.name;
@@ -17,7 +62,7 @@ function saveFields() {
 }
 
 //условие: если попап открыт, то закрываем, если закрыт, то открываем
-function clickButton() {
+function clickEdit() {
   if (popup.classList.contains('popup_opened') === true) {
     popup.classList.toggle('popup_opened');
   } else {
@@ -28,13 +73,100 @@ function clickButton() {
 //объявляем функцию для сохранения изменений в попапе
 function formSubmitHander(evt) {
   evt.preventDefault();
-
   formTitle.textContent = nameInput.value;
   formSubtitle.textContent = jobInput.value;
-  clickButton()
+  clickEdit();
 }
 
 //объявляем события по нажатию кнопок
 formElement.addEventListener('submit', formSubmitHander);
-popupExit.addEventListener('click', clickButton);
-editButton.addEventListener('click', clickButton);
+popupExit.addEventListener('click', clickEdit);
+editButton.addEventListener('click', clickEdit);
+
+function addCard() {
+  const template = itemTemplate.cloneNode(true);
+  template.querySelector('.element__img_add').src = formAdd.link;
+  template.querySelector('.element__img_add').alt = formAdd.placename;
+  template.querySelector('.element__header_add').textContent = formAdd.placename;
+
+  const favButton = template.querySelector('.element__fav');
+  favButton.addEventListener('click', (evt) => {
+    evt.target.classList.toggle('element__fav_active');
+  });
+
+  const delButton = template.querySelector('.element__del');
+  delButton.addEventListener('click', (evt) => {
+    let element = evt.target.closest('.element');
+    element.remove();
+  });
+  elements.prepend(template);
+};
+
+function formAddSaver (evt) {
+  evt.preventDefault();
+  addCard();
+  template.querySelector('.element__img_add').src = formAdd.link;
+  template.querySelector('.element__img_add').alt = formAdd.placename;
+
+  popupExitAdd.addEventListener('click', function () {
+    popupAdd.classList.toggle('popup_opened');
+  });
+  popupAddExit();
+};
+
+
+//Собираем карточку из шаблона
+function renderCard(card) {
+  const template = itemTemplate.cloneNode(true);
+  template.querySelector('.element__img').src = card.link;
+  template.querySelector('.element__img').alt = card.name;
+  template.querySelector('.element__header').textContent = card.name;
+
+  //Удаляем карточку по кнопке
+  const delButton = template.querySelector('.element__del');
+  delButton.addEventListener('click', (evt) => {
+    let element = evt.target.closest('.element');
+    element.remove();
+  });
+
+  //Добавляем в избранное
+  const favButton = template.querySelector('.element__fav');
+  favButton.addEventListener('click', (evt) => {
+    evt.target.classList.toggle('element__fav_active');
+  });
+
+  //Открываем превью
+  const elPreview = template.querySelector('.element__img_preview');
+  elPreview.addEventListener('click', (evt) => {
+    popupPreview.classList.toggle('popup_opened');
+    popupImg.src = evt.target.closest('.element__img').src;
+    popupImg.alt = evt.target.closest('.element__img').alt;
+    popupSubtitle.textContent = evt.target.closest('.element__img').alt;
+  });
+
+  //Добавляем в разметку
+  elements.append(template);
+};
+
+//Загружаем в карточки данные из массива
+initialCards.forEach(renderCard);
+
+//Закрываем превью
+popupExitPreview.addEventListener('click', function () {
+  popupPreview.classList.toggle('popup_opened');
+});
+
+//Вызываем попап добавления новой карточки
+addButton.addEventListener('click', function () {
+  popupAdd.classList.toggle('popup_opened');
+});
+
+function popupAddExit () {
+  popupAdd.classList.toggle('popup_opened');
+}
+
+//Закрываем попап добавления карточки
+popupExitAdd.addEventListener('click', popupAddExit);
+
+popupCreate.addEventListener('click', formAddSaver);
+
