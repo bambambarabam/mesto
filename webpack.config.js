631d4f213697deb21b7f81490f70392281eabad6
Slava Lebedintsev <xcarvedx@yandex.ru>
2022-02-14 13:18:47 +0300

    fix: npm update

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,32 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   entry: { main: './src/pages/index.js' },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'main.js'
+    filename: 'main.js',
+    publicPath: ''
   },
   mode: 'development',
-
+  devServer: {
+    static: path.resolve(__dirname, './dist'),
+    compress: true,
+    port: 8080,
+    open: true
+  },
   module: {
     rules: [
       {
         test: /\.js$/,
-        loader: 'babel-loader',
+        use: 'babel-loader',
         exclude: '/node_modules/'
       },
       {
         test: /\.css$/,
-        loader: [
+        use: [
           MiniCssExtractPlugin.loader,
           'css-loader',
           'postcss-loader'
@@ -27,22 +34,19 @@ module.exports = {
       },
       {
         test: /\.html$/,
-        loader: 'html-loader',
+        use: 'html-loader',
       },
       {
-        test: /\.(png|svg|jpg|gif)$/,
-        loader: 'file-loader?name=./images/[name].[ext]'
-      },
-      {
-        test: /\.(woff|woff2|ttf)$/,
-        loader: 'file-loader?name=./fonts/[name].[ext]'
-      },
+        test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
+        type: 'asset/resource'
+      }
     ]
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html'
     }),
+    new CleanWebpackPlugin(),
     new MiniCssExtractPlugin()
   ]
 };
